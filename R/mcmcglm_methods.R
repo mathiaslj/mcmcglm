1c50bcadeb18443e4894537e5db487a24054cf7b
Mathias Lerbech Jeppesen <mathiasljeppesen@outlook.com>
2024-12-08 17:15:15 -0800

    docs: Added an example to each of the mcmcglm S3 methods

--- R/mcmcglm_methods.R
+++ R/mcmcglm_methods.R
@@ -12,6 +12,33 @@ print.mcmcglm <- function(x, ...) {
 #'
 #' @param x an `mcmcglm` object
 #'
+#' @examples
+#' \dontrun{
+#' # Create test data
+#' n <- 100
+#' x1 <- rnorm (n)
+#' x2 <- rbinom (n, 1, .5)
+#' b0 <- 1
+#' b1 <- 1.5
+#' b2 <- 2
+#' lin_pred <- b0+b1*x1+b2*x2
+#'
+#' # For family "gaussian" and iid normal prior
+#' y_norm <- rnorm(n, mean = lin_pred, sd = 1)
+#' dat_norm <- data.frame(Y = y_norm, X1 = x1, X2 = x2)
+#'
+#' norm <- mcmcglm(formula = Y ~ .,
+#'                    data = dat_norm,
+#'                    beta_prior = distributional::dist_normal(0, 1),
+#'                    family = "gaussian",
+#'                    n_samples = 100,
+#'                    burnin = 10,
+#'                    sample_method = "slice_sampling",
+#'                    qslice_fun = qslice::slice_stepping_out,
+#'                    w = 0.5)
+#' samples(norm)
+#' }
+#'
 #' @export
 samples <- function(x) {
   UseMethod("samples")
@@ -26,6 +53,33 @@ samples.mcmcglm <- function(x) {
 #'
 #' @param x an `mcmcglm` object
 #'
+#' @examples
+#' \dontrun{
+#' # Create test data
+#' n <- 100
+#' x1 <- rnorm (n)
+#' x2 <- rbinom (n, 1, .5)
+#' b0 <- 1
+#' b1 <- 1.5
+#' b2 <- 2
+#' lin_pred <- b0+b1*x1+b2*x2
+#'
+#' # For family "gaussian" and iid normal prior
+#' y_norm <- rnorm(n, mean = lin_pred, sd = 1)
+#' dat_norm <- data.frame(Y = y_norm, X1 = x1, X2 = x2)
+#'
+#' norm <- mcmcglm(formula = Y ~ .,
+#'                    data = dat_norm,
+#'                    beta_prior = distributional::dist_normal(0, 1),
+#'                    family = "gaussian",
+#'                    n_samples = 100,
+#'                    burnin = 10,
+#'                    sample_method = "slice_sampling",
+#'                    qslice_fun = qslice::slice_stepping_out,
+#'                    w = 0.5)
+#' coef(norm)
+#' }
+#'
 #' @export
 coef.mcmcglm <- function(x) {
   x$beta_mean
@@ -35,7 +89,35 @@ coef.mcmcglm <- function(x) {
 #'
 #' @inheritParams samples
 #' @param samples_drop a `numeric` specifying a number of initial samples to
-#' exclude from the trace_plot to improve the axis zoom on the plot
+#' exclude from the trace_plot to improve the axis zoom on the plot. Defaults to
+#' drop halv of the burnin samples
+#'
+#' @examples
+#' \dontrun{
+#' # Create test data
+#' n <- 100
+#' x1 <- rnorm (n)
+#' x2 <- rbinom (n, 1, .5)
+#' b0 <- 1
+#' b1 <- 1.5
+#' b2 <- 2
+#' lin_pred <- b0+b1*x1+b2*x2
+#'
+#' # For family "gaussian" and iid normal prior
+#' y_norm <- rnorm(n, mean = lin_pred, sd = 1)
+#' dat_norm <- data.frame(Y = y_norm, X1 = x1, X2 = x2)
+#'
+#' norm <- mcmcglm(formula = Y ~ .,
+#'                    data = dat_norm,
+#'                    beta_prior = distributional::dist_normal(0, 1),
+#'                    family = "gaussian",
+#'                    n_samples = 100,
+#'                    burnin = 10,
+#'                    sample_method = "slice_sampling",
+#'                    qslice_fun = qslice::slice_stepping_out,
+#'                    w = 0.5)
+#' trace_plot(norm)
+#' }
 #'
 #' @export
 trace_plot <- function(x, samples_drop = NULL) {
